b57b183c01b7281dfafc53e70766694358d03163
Arne Sahlberg <arne.sahlberg@gmail.com>
2023-07-18 21:43:37 +0200

    works pretty good, I think

--- src/converter.rs
+++ src/converter.rs
@@ -4,12 +4,10 @@ use chrono::DateTime;
 use std::path::Path;
 
 
-pub fn convert_to_obsidian (input_folder : &str, output_folder : &str, use_icons : bool) -> Result<(), Box<dyn Error>> {
+pub fn convert_to_obsidian (input_folder : &str, output_folder : &str, use_icons : bool, tag_prefix : &str) -> Result<(), Box<dyn Error>> {
     let filepath = Path::new(input_folder).join("Journal.json");
     let mut file = File::open(filepath).unwrap();
     let mut contents = String::new();
-
-    let tag_prefix = "#journal/";
 
     file.read_to_string(&mut contents)?;
     let journal = serde_json::from_str::<Journal>(&contents)?;
@@ -65,35 +63,28 @@ pub fn convert_to_obsidian (input_folder : &str, output_folder : &str, use_icons
                         location.remove(0);
                         location.remove(0);
                     };
-                    Some(location)
+                    location
                 },
-                None => None
+                None => String::new()
             }
 
         };
 
-        let mut frontmatter = format!("---\n- created: {}", time_zone_date.format("%Y-%m-%d %H:%M:%S").to_string());
-        match entry.location {
-            Some(ref loc) => {
-                let coordinates = format!("[{},{}]", loc.latitude, loc.longitude);
-                frontmatter.push_str(&format!("\n- location: {}", coordinates));
-            },
-            None => (),
-        };
-        frontmatter.push_str("\n---");
 
-        new_entry.push_str(&frontmatter);
+
 
         // page header
         new_entry.push_str(
-          format!("\n\n# {}{}",
+          format!("# {}{}\n\n",
             date_icons,
-            time_zone_date.format( "%A, %d %B %Y at %-I:%M %p").to_string()
+            time_zone_date.format( "%A, %d %B %Y at %-I:%M %p\n\n").to_string()
           ).as_str()
         );
 
+ 
+
         // add body text
-        let text = 
+        let mut text = 
             match entry.text{
                 Some(ref text) => {
                     text
@@ -103,21 +94,20 @@ pub fn convert_to_obsidian (input_folder : &str, output_folder : &str, use_icons
                 None => String::new(),
                 
             };
-        new_entry.push_str(&format!("\n\n{}", text));
+
+        // don't push to new entry yet, but do after the photos part because we may want to put a photo at the top of the entry
 
         let year_folder =  Path::new(output_folder).join(time_zone_date.format("%Y").to_string());
         let month_folder = year_folder.join(time_zone_date.format("%m").to_string());
         // create monthfolder if not exists
         std::fs::create_dir_all(&month_folder)?;
         
- 
-        
         // put photos in the correct folder
         match entry.photos {
             Some(ref photos) => {
                 for photo in photos {
                     let file_name = format!("{}.jpeg", photo.md5) ;
-                    let new_file_name = format!("{}.jpeg", photo.identifier) ;
+                    let new_file_name = format!("{}.jpeg", photo.identifier) ; // all files have .jpeg extension afaik
                     let temp_path = { // older version had identifier instead of md5 as filename (and just a single file per entry)
                         let p = Path::new(input_folder).join("photos").join(&file_name);
                         if p.exists() {
@@ -140,40 +130,95 @@ pub fn convert_to_obsidian (input_folder : &str, output_folder : &str, use_icons
                             Some(path) => path,
                             None => "",
                         };
-                    let temp_path = month_folder.join(&new_file_name);
+                    let temp_path = month_folder.join("photos").join(&new_file_name);
                     let new_file_path =
                         match temp_path.to_str() {
                             Some(path) => path,
                             None => "",
                         };
                     
+                    // create folder if not exists
+                    std::fs::create_dir_all(month_folder.join("photos"))?;
                     println!("Copying {} to {}", file_path, new_file_path);
                     std::fs::copy(file_path, new_file_path)?;
 
-                    // replace part of the entry text to link to the file
-                    let old_link = format!("![](dayone-moment://{})", &file_name.replace(".jpeg", "")) ;
-                    let photo_link = format!("![]({})", new_file_name);
-                    new_entry = new_entry.replace(&old_link, &photo_link);
+                    // check if it's an entry from the older version where dayone-moment:// wasn't included in the file and
+                    // there was just a single photo per entry at most
+                    if text.contains("dayone-moment://") {
+                        // replace part of the entry text to link to the file
+                        let old_link = format!("![](dayone-moment://{})", &file_name.replace(".jpeg", "")) ; 
+                        let photo_link = format!("![](photos/{})", new_file_name);
+                        text = text.replace(&old_link, &photo_link);
+                    }
+                    else {
+                        // add a link to the photo at the end of the entry
+                        new_entry.push_str(&format!("\n\n![]({})", new_file_name));
+                    }
                 }
             },
             None => (),
         };
 
-        let tags = 
-            match entry.tags {
-                Some(ref tags) => {
-                    let mut tags_string = String::new();
-                    for tag in tags {
-                        tags_string.push_str(&format!("\n{}{}", tag_prefix, tag));
-                    };
-                    tags_string
+        new_entry.push_str(&text);
+
+
+        let mut frontmatter = format!("\n\n---\n- created: {}", time_zone_date.format("%Y-%m-%d %H:%M:%S").to_string());
+        match entry.location {
+            Some(ref loc) => {
+                let coordinates = format!("[{},{}]", loc.latitude, loc.longitude);
+                frontmatter.push_str(&format!("\n- location: {} {}", location, coordinates));
+            },
+            None => (),
+        };
+        frontmatter.push_str("\n---");
+        new_entry.push_str(&frontmatter);
+
+
+        let tags = {
+            let mut tags_string = String::new();
+            let all_tags =
+                match entry.tags {
+                    Some(ref tags) => {
+                        let mut tags_string = String::new();
+                        for tag in tags {
+                            let modified_tag = format!(" {}{}", tag_prefix, tag.replace(" ", "-").replace("---", "-"));
+                            tags_string.push_str(&modified_tag);
+                        };
+                        tags_string
+                    },
+                    None => String::new(),
+                };
+            tags_string.push_str(&all_tags);
+            match entry.starred {
+                true => {
+                    tags_string.push_str(" starred");
                 },
-                None => String::new(),
+                false => (),
             };
+            if tags_string.len() > 0 {
+                format!("- Tags:\n{}", tags_string)
+            }
+            else {
+                tags_string
+            }
+        } ;
+
+        new_entry.push_str(&format!("\n\n{}", tags));
+            
 
         // write the entry to the output folder
-        let file_name = format!("{}.md", entry.uuid);
-        println!("{}", new_entry);
+        let file_name = format!("{}.md", time_zone_date.format("%Y-%m-%d-%H-%M-%S"));
+        let file_path = month_folder.join(file_name);
+        let file_path = 
+            match file_path.to_str() {
+                Some(path) => path,
+                None => "",
+            };
+
+        println!("Writing to {}", file_path);
+        let mut file = File::create(file_path)?;
+        file.write_all(new_entry.as_bytes())?;
+
     };
     println!("Done processing entries.");
 
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,31 @@ pub mod types;
 use types::Journal;
 pub mod converter;
 use converter::convert_to_obsidian;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "converter")]
+
+struct Flags {
+    /// Input folder
+    #[structopt(short = "i", long = "input-folder")]
+    input_folder: String,
+
+    /// Output folder
+    #[structopt(short = "o", long = "output-folder")]
+    output_folder: String,
+
+    /// if the Icons Plugin is used?
+    #[structopt(long = "icons")]
+    icons: bool,
+
+    #[structopt(long = "tag-prefix", short="t", default_value = "#journal/")]
+    tag_prefix: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input_folder = "/Users/arnesahlberg/Downloads/07-16-2023_9-33-em";
-    let output_folder = "/Users/arnesahlberg/Downloads/07-16-2023_9-33-em-obsidian";
-    convert_to_obsidian(input_folder, output_folder, true)?;
+    let flags = Flags::from_args();
+
+    convert_to_obsidian(&flags.input_folder, &flags.output_folder, flags.icons, &flags.tag_prefix)?;
     Ok(())
 }
